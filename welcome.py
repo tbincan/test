4347fd73d81f40b4aed61987e783c4ee6283dad9
tbincan <tbincan@gmail.com>
2023-04-24 23:00:40 +0300

    added second line

--- welcome.py
+++ welcome.py
@@ -3,6 +3,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
         return "<h1>WELCOME TO TURAN CYBER HUB</h1>"
+               "<h2>Git Class Hands On</h2>"
     if __name__ == "__main__":
             app.run(host="0.0.0.0", port=80)
 
