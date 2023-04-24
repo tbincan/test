d74555f2ac5504ed21230229ef4b00b8305bd682
tbincan <tbincan@gmail.com>
2023-04-24 22:08:43 +0300

    my second commit

--- welcome.py
+++ welcome.py
@@ -2,7 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-        return "<h1>Welcome to Turan Cyber Hub</h1>"
+        return "<h1>WELCOME TO TURAN CYBER HUB</h1>"
     if __name__ == "__main__":
             app.run(host="0.0.0.0", port=80)
+
+
 
