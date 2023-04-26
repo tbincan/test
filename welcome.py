0350a1d159b68466b764af3273af9ec51ae68335
tbincan <tbincan@gmail.com>
2023-04-26 20:48:12 +0300

    Revert "my second commit"
    This reverts commit d74555f2ac5504ed21230229ef4b00b8305bd682.

--- welcome.py
+++ welcome.py
@@ -2,10 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
+<<<<<<< HEAD
         return "<h1>WELCOME TO TURAN CYBER HUB</h1>"
                "<h2>Git Class Hands On</h2>"
     if __name__ == "__main__":
             app.run(host="0.0.0.0", port=80)
-
-
 
